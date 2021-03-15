e6a89437a2dc1448f0dd4067cbd2d4ff5415bd05
mouadlouhibi2000@gmail.com <Math-2019>
2021-03-15 15:33:17 +0100

    Componenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GestionComponent } from './components/gestion/gestion.component';
+import { ListEtudiantComponent } from './components/list-etudiant/list-etudiant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GestionComponent,
+    ListEtudiantComponent
   ],
   imports: [
     BrowserModule,
